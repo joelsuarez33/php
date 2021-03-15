5f765c6376b6974d12f0f363fae1f7955a0d36dc
Joel Suarez <68664450+joelsuarez33@users.noreply.github.com>
2021-03-14 21:57:54 -0300

    Update tablas.sql

--- tablas.sql
+++ tablas.sql
@@ -5,8 +5,24 @@ apellidos   varchar(100) not null,
 email       varchar(255) not null,
 password    varchar(255),
 fecha       date not null,
-CONSTRAINT pk_usuarios PRIMARY KEY(id)
-)
+CONSTRAINT pk_usuarios PRIMARY KEY(id),
+CONSTRAINT uq_email UNIQUE(email)
+)ENGINE=InnoDb;
 
 CREATE TABLE categorias(
 id          int(255) auto_increment not null,
+nombre      varchar(100),
+CONSTRAINT PK_categorias PRIMARY KEY(id)
+);ENGINE=InnoDb;
+
+CREATE TABLE entradas(
+id              int(255) auto_increment not null,
+usuario_id      int(255) not null,
+categoria_id    int(255) not null,
+titulo          varchar(255) not null,
+descripcion     MEDIUMTEXT,
+fecha           date not null,
+CONSTRAINT pk_entradas PRIMARY KEY(id),
+CONSTRAINT fk_entrada_usuario FOREIGN KEY(usuario_id) REFERENCES usuarios(id),
+CONSTRAINT fk_entrada_categoria FOREIGN KEY(categoria_id) REFERENCES categoria(id)
+);
